1ae981630b0a6e3faa886fb3d15e2ea5aa6c7dcc
Leo <295577311@qq.com>
2020-11-26 15:13:28 +1300

    All done

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import "../styles/globals.css";
+import { makeServer } from "../mirage/mirage";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
